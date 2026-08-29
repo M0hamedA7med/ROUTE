a2dfc0161b8d6cfd67df6f596014bb249227a515
Mohamed Ahmed Elsayed Morsy <161088706+M0hamedA7med@users.noreply.github.com>
2026-08-29 18:12:07 +0300

    Add files via upload

---
+++ Book.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment
+{
+    public class Book
+    {
+        public string Title { get; set; }
+        public int Pages { get; set; }
+
+        public Book(string title, int pages)
+        {
+            Title = title;
+            Pages = pages;
+        }
+        public override string ToString()
+        {
+            return $"Book Title: {Title}, Pages: {Pages}";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using Assignment;
+
+//first one 
+Book book = new Book("Asp.Net Basics", 400);
+object obj = book;
+Console.WriteLine(obj);
+
+
+//second one 
+Console.WriteLine(book.ToString());
+Console.WriteLine(book.Equals(book));
+Console.WriteLine(book.GetHashCode());
+Console.WriteLine(book.GetType());
+
+
+//third one 
+
+//Error type: Compile-time errorConsole.WriteLine("Explanation: Cannot assign a string literal to an integer variable.");
+int pagesFix = 464;
+
+
+
+//number four
+try
+{
+    int zero = 0;
+    int result = 10 / zero;
+}
+catch (DivideByZeroException)
+{
+    Console.WriteLine("Cannot divide by zero");
+}
+finally
+{
+    Console.WriteLine("Done");
+}
+
+//number 5
+int pages = 300;
+double doublePages = pages;
+Console.WriteLine($"Implicit conversion (double): {doublePages}");
+
+
